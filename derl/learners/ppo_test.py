b3059852d8821b90fce5d6a8761c58a6f37918ff
MichaelKonobeev <konobeev.michael@gmail.com>
2019-11-21 12:51:22 -0700

    Add learner_test.py

--- derl/learners/ppo_test.py
+++ derl/learners/ppo_test.py
@@ -1,28 +1,13 @@
 # pylint: disable=missing-docstring
-import random
-import tempfile
-from unittest import TestCase
-import numpy as np
 import tensorflow as tf
+from derl.learners.learner_test import TestLearner
 from derl.learners.ppo import PPOLearner
-from derl.env.make_env import make as make_env
 
 tf.enable_eager_execution()
 
 
-class TestPPOLearner(TestCase):
-  def setUp(self):
-    random.seed(0)
-    np.random.seed(0)
-    tf.random.set_random_seed(0)
-
+class TestPPOLearner(TestLearner):
   def test_atari(self):
-    kwargs = PPOLearner.get_kwargs("atari")
-    env = make_env("SpaceInvadersNoFrameskip-v4", nenvs=kwargs["nenvs"], seed=0)
-    learner = PPOLearner.make_with_kwargs(env, **kwargs)
-    learner.model.load_weights("testdata/ppo-atari/init-weights.hdf5")
-
-    num_test_steps = 12
     expected = [
         0.03779646381735802,
         0.038728635758161545,
@@ -38,9 +23,4 @@ class TestPPOLearner(TestCase):
         0.0044595301151275635,
         0.02526124007999897,
     ]
-    logdir = tempfile.gettempdir()
-    for step, (_, loss) in enumerate(learner.learning_generator(logdir, 1)):
-      with self.subTest(step=step):
-        self.assertEqual(float(loss), expected[step])
-      if step == num_test_steps:
-        break
+    self.assert_loss_values(PPOLearner, "SpaceInvadersNoFrameskip-v4", expected)
