44e7473dc456d556d0184986d39f18798b73005d
MichaelKonobeev <konobeev.michael@gmail.com>
2019-12-11 16:21:51 -0700

    Add models_torch.py

---
+++ derl/models_torch.py
@@ -0,0 +1,128 @@
+""" PyTorch models for RL. """
+from functools import partial
+from math import floor
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+def conv2d_output_shape(height, width, conv2d):
+  """ Computes output shape of given conv2d layer. """
+  padding, stride = conv2d.padding, conv2d.stride
+  dilation, kernel_size = conv2d.dilation, conv2d.kernel_size
+  out_height = floor(
+      (height + 2 * padding[0] - dilation[0] * (kernel_size[0] - 1) - 1)
+      / stride[0] + 1)
+  out_width = floor(
+      (width + 2 * padding[1] - dilation[1] * (kernel_size[1] - 1) - 1)
+      / stride[1] + 1)
+  return out_height, out_width
+
+
+class NatureDQNBase(nn.Sequential):
+  """ Hidden layers of the Nature DQN model. """
+  def __init__(self, input_shape=(84, 84, 4), permute=True):
+    super().__init__()
+    self.permute = permute
+    in_channels, height, width = input_shape
+    if permute:
+      height, width, in_channels = input_shape
+    convolutions = [
+        nn.Conv2d(in_channels, 32, 8, 4),
+        nn.Conv2d(32, 64, 4, 2),
+        nn.Conv2d(64, 64, 3, 1),
+    ]
+    for i, conv in enumerate(convolutions):
+      height, width = conv2d_output_shape(height, width, conv)
+      self.add_module(f"conv-{i}", conv)
+      self.add_module(f"relu-{i}", nn.ReLU())
+
+    self.add_module("flatten", nn.Flatten())
+    in_features = height * width * convolutions[-1].out_channels
+    self.add_module("linear", nn.Linear(in_features, 512))
+
+  def forward(self, inputs):  # pylint: disable=arguments-differ
+    if isinstance(inputs, np.ndarray):
+      inputs = torch.from_numpy(inputs)
+    if self.permute:
+      inputs = inputs.permute(0, 3, 1, 2)
+    if inputs.dtype == torch.uint8:
+      inputs = inputs.float() / 255
+    inputs = inputs.contiguous()
+    return super().forward(inputs)
+
+
+def init_weights(layer, weight_initializer, bias_initializer):
+  """ Initializers weights and biases of a given layer. """
+  if hasattr(layer, "weight"):
+    weight_initializer(layer.weight)
+  if hasattr(layer, "bias"):
+    bias_initializer(layer.bias)
+
+
+def broadcast_inputs(ndims):
+  """ Broadcast inputs to specified number of dims and then back. """
+  def decorator(forward):
+    def wrapped(self, inputs):
+      expand_dims = ndims - len(inputs.shape)
+      inputs = inputs[(None,) * expand_dims]
+      outputs = forward(self, inputs)
+      if expand_dims:
+        if isinstance(outputs, (tuple, list)):
+          return type(outputs)(
+              map(lambda t: torch.reshape(t, t.shape[expand_dims:]),
+                  outputs)
+          )
+        return torch.reshape(outputs, outputs.shape[expand_dims:])
+      return outputs
+    return wrapped
+  return decorator
+
+
+class NatureDQN(nn.Module):
+  """ Nature DQN model that supports subsequently proposed modifications. """
+  def __init__(self,
+               output_units,
+               input_shape=(84, 84, 4),
+               dueling=False,
+               nbins=None,
+               weight_initializer=nn.init.orthogonal_,
+               bias_initializer=nn.init.zeros_):
+    super().__init__()
+    self.dueling = dueling
+    self.nbins = nbins
+    self.single_output = not isinstance(output_units, (list, tuple))
+    self.output_units = ([output_units] if self.single_output
+                         else list(output_units))
+
+    if nbins is not None:
+      self.output_units[0] *= nbins
+    if dueling:
+      self.output_units.append(nbins or 1)
+
+    self.base = NatureDQNBase(input_shape)
+    in_units = list(self.base.children())[-1].out_features
+    self.output_layers = nn.ModuleList(
+        [nn.Linear(in_units, out_units)
+         for out_units in self.output_units])
+    self.apply(partial(init_weights,
+                       weight_initializer=weight_initializer,
+                       bias_initializer=bias_initializer))
+
+  @broadcast_inputs(ndims=4)
+  def forward(self, inputs):  # pylint: disable=arguments-differ
+    base_outputs = self.base(inputs)
+    outputs = [layer(base_outputs) for layer in self.output_layers]
+    if self.nbins is not None:
+      nactions = self.output_units[0] // self.nbins
+      outputs[0] = torch.reshape(outputs[0], (-1, nactions, self.nbins))
+    if self.dueling:
+      advantages, values = outputs[0], outputs.pop()
+      values = torch.reshape(values,
+                             (-1, 1, self.nbins) if self.nbins is not None
+                             else (-1, 1))
+      outputs[0] = (values + advantages
+                    - torch.mean(advantages, 1, keepdims=True))
+    if self.single_output:
+      outputs = outputs[0]
+    return outputs
---
+++ derl/models_torch_test.py
@@ -0,0 +1,56 @@
+from unittest import TestCase
+import numpy as np
+import numpy.testing as nt
+import torch
+from derl.models_torch import NatureDQNBase, NatureDQN
+
+torch.manual_seed(0)
+
+
+class DQNBaseTest(TestCase):
+  def test_call(self):
+    inputs = torch.rand(32, 84, 84, 4)
+    dqn_base = NatureDQNBase()
+    outputs = dqn_base(inputs)
+    expected = np.load("testdata/models/dqn-base-outputs.npy")
+    nt.assert_allclose(outputs.detach().numpy(), expected)
+
+
+class NatureDQNForActorCriticTest(TestCase):
+  def setUp(self):
+    self.dqn = NatureDQN(output_units=(4, 1))
+
+  def test_params(self):
+    nweights = nbiases = 0
+    for module in self.dqn.modules():
+      if hasattr(module, "bias"):
+        nt.assert_equal(module.bias.detach().numpy(), 0.)
+        nbiases += 1
+      if hasattr(module, "weight"):
+        weight = module.weight.detach().numpy()
+        weight = np.reshape(weight, (weight.shape[0], -1))
+        nrows, ncols = weight.shape
+        if nrows > ncols:
+          nt.assert_allclose(weight.T @ weight, np.eye(ncols), atol=1e-6)
+        else:
+          nt.assert_allclose(weight @ weight.T, np.eye(nrows), atol=1e-6)
+        nweights += 1
+    self.assertEqual(nweights, 6)
+    self.assertEqual(nbiases, 6)
+
+
+  def test_broadcast(self):
+    inputs = torch.rand(84, 84, 4)
+    outputs = self.dqn(inputs)
+    self.assertEqual(len(outputs), 2)
+    self.assertEqual(outputs[0].shape, torch.Size((4,)))
+    self.assertEqual(outputs[1].shape, torch.Size((1,)))
+
+
+class NatureDQNForDistributionalRLTest(TestCase):
+  def setUp(self):
+    self.dqn = NatureDQN(output_units=6, nbins=51)
+
+  def test_output_shape(self):
+    outputs = self.dqn(torch.rand(32, 84, 84, 4))
+    self.assertEqual(outputs.shape, torch.Size([32, 6, 51]))
