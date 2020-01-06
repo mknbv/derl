f0939712392d4becdbdfb17cbe0de4e88cd8c15a
MichaelKonobeev <konobeev.michael@gmail.com>
2020-01-06 13:35:18 -0700

    Make A2CLearner test work on a different platform

--- derl/learners_torch/a2c_test.py
+++ derl/learners_torch/a2c_test.py
@@ -1,4 +1,5 @@
 # pylint: disable=missing-docstring
+import torch
 from derl.env.make_env_torch import make as make_env
 from derl.learners_torch.a2c import A2CLearner
 from derl.learners_torch.learner_test import LearnerTestCase
@@ -12,9 +13,18 @@ class A2CLearnerTest(LearnerTestCase):
     self.env = make_env("SpaceInvadersNoFrameskip-v4",
                         nenvs=kwargs.get("nenvs"), seed=0)
     self.learner = A2CLearner.make_with_kwargs(self.env, **kwargs)
+    self.learner.model.load_state_dict(
+        torch.load("testdata/a2c-torch-atari/model.pt"))
+    self.learner.model.to("cpu")
+
+  def test_interactions(self):
+    self.assert_interactions("testdata/a2c-torch-atari/interactions.npz",
+                             rtol=1e-6, atol=1e-6)
 
   def test_grad(self):
-    self.assert_grad("testdata/a2c-torch-atari/grads.npz")
+    self.assert_grad("testdata/a2c-torch-atari/grads.npz",
+                     rtol=1e-6, atol=1e-6)
 
   def test_losses(self):
-    self.assert_losses("testdata/a2c-torch-atari/losses.npy")
+    self.assert_losses("testdata/a2c-torch-atari/losses.npy",
+                       rtol=1e-5, atol=1e-4)
