c41d4aa416e4d335b001fda7649cec30fe92a91b
MichaelKonobeev <konobeev.michael@gmail.com>
2020-01-22 08:10:27 -0700

    Add get_tensor method to AnnealingVariable

--- derl/anneal.py
+++ derl/anneal.py
@@ -17,8 +17,12 @@ class AnnealingVariable(ABC):
     self.name = name or camel2snake(self.__class__.__name__)
 
   @abstractmethod
-  def get_value(self):
+  def get_tensor(self):
+    """ Returns torch.Tensor that changes after each call to step. """
+
+  def get_current_value(self):
     """ Returns the current value of the variable. """
+    return torch.tensor(self.get_tensor())  # pylint: disable=not-callable
 
   @abstractmethod
   def step(self):
@@ -26,7 +30,7 @@ class AnnealingVariable(ABC):
 
   def summarize(self, global_step):
     """ Writes summary of the value for tensorboard. """
-    summary.add_scalar(self.name, self.get_value(), global_step=global_step)
+    summary.add_scalar(self.name, self.get_tensor(), global_step=global_step)
 
 
 class TorchSched(AnnealingVariable):
@@ -34,14 +38,17 @@ class TorchSched(AnnealingVariable):
   def __init__(self, scheduler, name=None):
     super().__init__(name)
     self.scheduler = scheduler
-
-  def get_value(self):
     # pylint: disable=not-callable
-    return torch.tensor(self.scheduler.get_last_lr())
+    self.tensor = torch.tensor(self.scheduler.get_last_lr())
+
+  def get_tensor(self):
+    return self.tensor
 
   def step(self):
     self.scheduler.step()
-    return self.get_value()
+    # pylint: disable=not-callable
+    self.tensor.data = torch.tensor(self.scheduler.get_last_lr())
+    return self.get_current_value()
 
 
 class LinearAnneal(AnnealingVariable):
@@ -52,18 +59,18 @@ class LinearAnneal(AnnealingVariable):
     self.end = end
     self.step_count = 0
     self.num_steps = num_steps
-    self.value = torch.tensor(self.start)  # pylint: disable=not-callable
+    self.tensor = torch.tensor(self.start)  # pylint: disable=not-callable
 
-  def get_value(self):
-    return self.value
+  def get_tensor(self):
+    return self.tensor
 
   def step(self):
     self.step_count += 1
     step_frac = self.step_count / self.num_steps
-    self.value = torch.clamp(
+    self.tensor.data = torch.clamp(
         # pylint: disable=not-callable
         torch.tensor(self.start + (self.end - self.start) * step_frac),
         min(self.start, self.end),
         max(self.start, self.end)
     )
-    return self.value
+    return self.get_current_value()
