19a83f1bed00994a0a0b8deaa03378bc61c3ae29
MichaelKonobeev <konobeev.michael@gmail.com>
2020-07-07 12:27:31 -0600

    Add derl/factory/__init__.py

---
+++ derl/factory/__init__.py
@@ -0,0 +1,5 @@
+""" derl.factory subpackage initialization. """
+from derl.factory.factory import Factory
+from derl.factory.a2c import A2CFactory
+from derl.factory.dqn import DQNFactory
+from derl.factory.ppo import PPOFactory
