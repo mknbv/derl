5644c841b67fa5ae497b6b96b18a356f4b29e390
MichaelKonobeev <konobeev.michael@gmail.com>
2020-07-07 10:35:16 -0600

    Add PPOFactory

---
+++ derl/alg/ppo_test.py
@@ -0,0 +1,53 @@
+# pylint: disable=missing-docstring
+import torch
+from derl.env.make_env import make as make_env
+from derl.factory.ppo import PPOFactory
+from derl.alg.test import AlgTestCase
+
+
+class PPOLearnerAtariTest(AlgTestCase):
+  def setUp(self):
+    super().setUp()
+
+    kwargs = PPOFactory.get_kwargs()
+    self.env = make_env("BreakoutNoFrameskip-v4",
+                        nenvs=kwargs.get("nenvs"), seed=0)
+    self.alg = PPOFactory(**kwargs).make(self.env)
+    self.alg.model.load_state_dict(
+        torch.load("testdata/ppo/atari/model.pt"))
+    self.alg.model.to("cpu")
+
+  def test_interactions(self):
+    self.assert_interactions("testdata/ppo/atari/interactions.npz",
+                             rtol=1e-6, atol=1e-6)
+
+  def test_grad(self):
+    self.assert_grad("testdata/ppo/atari/grads.npz", rtol=1e-6, atol=1e-6)
+
+  def test_losses(self):
+    self.assert_losses("testdata/ppo/atari/losses.npy", rtol=1e-5, atol=1e-5)
+
+
+class PPOLearnerPyBulletTest(AlgTestCase):
+  def setUp(self):
+    super().setUp()
+
+    kwargs = PPOFactory.get_kwargs("mujoco")
+    # Modify some hyper parameters in order for the test not to take to long
+    kwargs["num_runner_steps"] = 12
+    kwargs["num_minibatches"] = 2
+    kwargs["num_epochs"] = 3
+    self.env = make_env("HalfCheetahBulletEnv-v0",
+                        nenvs=kwargs.get("nenvs"), seed=0)
+    self.alg = PPOFactory(**kwargs).make(self.env)
+    self.alg.model.to("cpu")
+
+  def test_interactions(self):
+    self.assert_interactions("testdata/ppo/pybullet/interactions.npz",
+                             rtol=1e-5, atol=1e-5)
+
+  def test_grad(self):
+    self.assert_grad("testdata/ppo/pybullet/grads.npz", rtol=1e-5, atol=1e-5)
+
+  def test_losses(self):
+    self.assert_losses("testdata/ppo/pybullet/losses.npy", rtol=1e-5, atol=1e-5)
---
+++ derl/factory/ppo.py
@@ -0,0 +1,79 @@
+""" Implements PPO factory. """
+from torch.optim import Adam
+from derl.alg.common import Trainer
+from derl.anneal import LinearAnneal
+from derl.factory.factory import Factory
+from derl.models import make_model
+from derl.policies import ActorCriticPolicy
+from derl.alg.ppo import PPO
+from derl.runners.onpolicy import make_ppo_runner
+
+
+class PPOFactory(Factory):
+  """ Proximal Policy Optimization learner. """
+  @staticmethod
+  def get_parser_defaults(args_type="atari"):
+    defaults = {
+        "atari": {
+            "num-train-steps": 10e6,
+            "nenvs": 8,
+            "num-runner-steps": 128,
+            "gamma": 0.99,
+            "lambda_": 0.95,
+            "num-epochs": 3,
+            "num-minibatches": 4,
+            "cliprange": 0.1,
+            "value-loss-coef": 0.25,
+            "entropy-coef": 0.01,
+            "max-grad-norm": 0.5,
+            "lr": 2.5e-4,
+            "optimizer-epsilon": 1e-5,
+        },
+        "mujoco": {
+            "num-train-steps": 1e6,
+            "nenvs": dict(type=int, default=None),
+            "num-runner-steps": 2048,
+            "gamma": 0.99,
+            "lambda_": 0.95,
+            "num-epochs": 10,
+            "num-minibatches": 32,
+            "cliprange": 0.2,
+            "value-loss-coef": 0.25,
+            "entropy-coef": 0.,
+            "max-grad-norm": 0.5,
+            "lr": 3e-4,
+            "optimizer-epsilon": 1e-5,
+        }
+    }
+    return defaults.get(args_type)
+
+  def make_runner(self, env, nlogs=1e5, **kwargs):
+    with self.custom_kwargs(**kwargs):
+      model = (self.get_arg("model") if self.has_arg("model")
+               else make_model(env.observation_space, env.action_space, 1))
+      policy = ActorCriticPolicy(model)
+      runner_kwargs = self.get_arg_dict("gamma", "lambda_",
+                                        "num_epochs", "num_minibatches")
+      runner = make_ppo_runner(env, policy, self.get_arg("num_runner_steps"),
+                               self.get_arg("num_train_steps"), nlogs=nlogs,
+                               **runner_kwargs)
+      return runner
+
+  def make_trainer(self, runner, **kwargs):
+    with self.custom_kwargs(**kwargs):
+      lr = LinearAnneal(*self.get_arg_list("lr", "num_train_steps"), name="lr")
+      params = runner.policy.model.parameters()
+      optimizer_kwargs = {"params": params, "lr": lr.get_tensor()}
+      if self.has_arg("optimizer_epsilon"):
+        optimizer_kwargs["eps"] = self.get_arg("optimizer_epsilon")
+      optimizer = Adam(**optimizer_kwargs)
+      return Trainer(optimizer, anneals=[lr],
+                     max_grad_norm=self.get_arg("max_grad_norm"))
+
+  def make_alg(self, runner, trainer, **kwargs):
+    with self.custom_kwargs(**kwargs):
+      ppo_kwargs = self.get_arg_dict("value_loss_coef",
+                                     "entropy_coef",
+                                     "cliprange")
+      ppo = PPO(runner, trainer, **ppo_kwargs)
+      return ppo
