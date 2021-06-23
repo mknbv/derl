ee6262d5881792ab54e2a8470e3e82273f23d4bd
MichaelKonobeev <konobeev.michael@gmail.com>
2021-06-23 16:17:52 -0600

    Update required versions in setup.py

--- setup.py
+++ setup.py
@@ -13,10 +13,11 @@ setup(
     install_requires=[
         "gym[atari]>=0.11,<0.15",
         "pybullet",
+        "scipy==1.5.0",
         "numpy==1.16.4",
         "opencv-python",
         "tensorboard==1.14",
-        "torch>=1.4.0,!=1.6.0",
+        "torch>=1.4.0,<1.6.0",
         "tqdm",
     ],
     long_description="DERL is a Deep Reinforcement Learning package"
