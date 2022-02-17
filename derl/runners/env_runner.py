cc1e8a9c67b9e3aaf5545fdc4afb5a2c8372d5e6
MichaelKonobeev <konobeev.michael@gmail.com>
2022-02-17 14:53:05 +0100

    EnvRunner supports time_limit arg

--- derl/runners/env_runner.py
+++ derl/runners/env_runner.py
@@ -5,12 +5,19 @@ from collections import defaultdict
 
 class EnvRunner:
   """ Iterable that interacts with an env. """
-  def __init__(self, env, policy, horizon, nsteps=None):
+  def __init__(self, env, policy, horizon, nsteps=None, time_limit=None):
     self.env = env
     self.policy = policy
     self.horizon = horizon
     self.nsteps = int(nsteps)
+    if (time_limit is not None
+        and getattr(self.env.unwrapped, "nenvs", None) is not None):
+      raise TypeError("batched envs are not supported for time_limit "
+                      f"not equal to None, got env={self.env}, "
+                      f"time_limit={time_limit}")
+    self.time_limit = time_limit
     self.step_count = 0
+    self.episode_length = 0
 
   @property
   def nenvs(self):
@@ -30,6 +37,7 @@ class EnvRunner:
     """ Interacts with the environment starting from obs for horizon steps. """
     if obs is None:
       obs = self.env.reset()
+      self.episode_length = 0
     while not self.is_exhausted():
       interactions = defaultdict(list)
       for _ in range(self.horizon):
@@ -41,6 +49,7 @@ class EnvRunner:
         for key, val in act.items():
           interactions[key].append(val)
         new_obs, rew, done, info = self.env.step(act["actions"])
+        self.episode_length += 1
         interactions["rewards"].append(rew)
         interactions["resets"].append(done)
         interactions["infos"].append(info)
@@ -48,7 +57,12 @@ class EnvRunner:
 
         # Note that batched envs should auto-reset, hence we only check
         # done flag if the env is not batched.
-        obs = self.env.reset() if self.nenvs is None and done else new_obs
+        if self.nenvs is None and (
+            done or self.episode_length == self.time_limit):
+          obs = self.env.reset()
+          self.episode_length = 0
+        else:
+          obs = new_obs
 
       interactions["state"] = dict(latest_observations=obs)
       self.step_count += self.horizon * (self.nenvs or 1)
